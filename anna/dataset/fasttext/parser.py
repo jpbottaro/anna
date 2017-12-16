414af013b3a062604b82d86aac55a83c1af9f16a
Juan Pablo Bottaro <jpbottaro@gmail.com>
2017-12-16 23:01:25 +0000

    dataset: fetcher and parser for fastText's word embeddings

---
+++ anna/dataset/fasttext/parser.py
@@ -0,0 +1,58 @@
+"""Reads the fasttext word embeddings
+
+Visit: https://fasttext.cc/docs/en/english-vectors.html#format"""
+
+import os
+import numpy as np
+import dataset.utils as utils
+from . import fetcher
+
+NAME = "wiki-news-300d-1M.vec"
+
+
+def fetch_and_parse(data_dir):
+    """
+    Fetches and parses the fasttext word embeddings dataset. The dataset is
+    also cached as a pickle for further calls.
+
+    Args:
+        data_dir (str): absolute path to the dir where datasets are stored
+
+    Returns:
+        voc (dict): bimap of word <-> id
+        emb (numpy array): array of embeddings for each word in `voc`
+    """
+    fasttext_dir = fetcher.fetch(data_dir)
+
+    return parse(fasttext_dir)
+
+
+@utils.cache(2)
+def parse(fasttext_dir):
+    """
+    Parses the fasttext word embeddings.
+
+    Args:
+        fastext_dir (str): absolute path to the extracted word embeddings
+
+    Returns:
+        voc (dict): bimap of word <-> id
+        emb (numpy.array): array of embeddings for each word in `voc`
+    """
+    voc = {}
+    emb = []
+    first = True
+    fasttext_path = os.path.join(fasttext_dir, NAME)
+    with open(fasttext_path) as f:
+        for line in f:
+            # First line contains # words and embedding sizes, skip
+            if first:
+                first = False
+                continue
+
+            i = len(emb)
+            parts = line.split(" ")
+            voc[parts[0]] = i
+            voc[i] = parts[0]
+            emb.append([float(n) for n in parts[1:-1]])
+    return voc, np.array(emb)
