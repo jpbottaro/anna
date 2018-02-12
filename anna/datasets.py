be8a56133425bca377757f8d1970aacd07b90b52
Juan Pablo Bottaro <jpbottaro@gmail.com>
2018-02-12 08:49:57 -0800

    dataset: migrate to 'anna' directory

--- anna/datasets.py
+++ anna/datasets.py
@@ -2,42 +2,42 @@
 
 import os
 import sys
-import dataset.aida.fetcher
-import dataset.ud.fetcher
-import dataset.conll03.fetcher
-import dataset.wmt.fetcher
-import dataset.wiki.fetcher
-import dataset.rcv1.fetcher
-import dataset.reuters21578.fetcher
+import anna.dataset.aida.fetcher
+import anna.dataset.ud.fetcher
+import anna.dataset.conll03.fetcher
+import anna.dataset.wmt.fetcher
+import anna.dataset.wiki.fetcher
+import anna.dataset.rcv1.fetcher
+import anna.dataset.reuters21578.fetcher
 
 options = [
     {
         "name": "Universal Dependencies (Morph, POS Tags)",
-        "fetcher": dataset.ud.fetcher.fetch
+        "fetcher": anna.dataset.ud.fetcher.fetch
     },
     {
         "name": "CoNLL03 (NER)",
-        "fetcher": dataset.conll03.fetcher.fetch
+        "fetcher": anna.dataset.conll03.fetcher.fetch
     },
     {
         "name": "WMT14 (MT)",
-        "fetcher": dataset.wmt.fetcher.fetch
+        "fetcher": anna.dataset.wmt.fetcher.fetch
     },
     {
         "name": "AIDA (Entity Linking)",
-        "fetcher": dataset.aida.fetcher.fetch
+        "fetcher": anna.dataset.aida.fetcher.fetch
     },
     {
         "name": "RCV1 (Multi-label Classification)",
-        "fetcher": dataset.rcv1.fetcher.fetch
+        "fetcher": anna.dataset.rcv1.fetcher.fetch
     },
     {
         "name": "Reuters-21578 (Multi-label Classification)",
-        "fetcher": dataset.reuters21578.fetcher.fetch
+        "fetcher": anna.dataset.reuters21578.fetcher.fetch
     },
     {
         "name": "Wiki",
-        "fetcher": dataset.wiki.fetcher.fetch
+        "fetcher": anna.dataset.wiki.fetcher.fetch
     }
 ]
 
