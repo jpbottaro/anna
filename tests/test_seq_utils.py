74fa236d7bca3a076f4d021d79628b58201db005
Juan Pablo Bottaro <jpbottaro@gmail.com>
2018-05-30 03:54:20 +0100

    Fix test failures after refactor

--- tests/test_seq_utils.py
+++ tests/test_seq_utils.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import anna.model.encode as encode
+import anna.model.utils as utils
 
 
 class SequenceUtilsTest(tf.test.TestCase):
@@ -7,7 +7,7 @@ class SequenceUtilsTest(tf.test.TestCase):
         with self.test_session():
             x = tf.constant([[0, 1, 0], [1, 0, 1], [0, 0, 1]])
             size = tf.constant([1, 0, 3])
-            y = encode.seq_roll(x, size)
+            y = utils.seq_roll(x, size)
 
             self.assertAllEqual(y.eval(), [[0, 0, 1], [1, 0, 1], [0, 0, 1]])
 
@@ -16,9 +16,9 @@ class SequenceUtilsTest(tf.test.TestCase):
             x = tf.constant([[0, 1, 0], [1, 0, 1], [0, 0, 1]])
             x = tf.expand_dims(x, axis=-1)
 
-            small = encode.seq_pad(x, 1)
-            same = encode.seq_pad(x, 3)
-            large = encode.seq_pad(x, 5)
+            small = utils.seq_pad(x, 1)
+            same = utils.seq_pad(x, 3)
+            large = utils.seq_pad(x, 5)
 
             self.assertAllEqual(small.eval(), [[[0]], [[1]], [[0]]])
             self.assertAllEqual(same.eval(), x.eval())
@@ -36,7 +36,7 @@ class SequenceUtilsTest(tf.test.TestCase):
             y = tf.expand_dims(y, axis=-1)
             y_len = tf.constant([2, 1, 0])
 
-            res, res_len = encode.seq_concat([x, y], [x_len, y_len])
+            res, res_len = utils.seq_concat([x, y], [x_len, y_len])
 
             self.assertAllEqual(res.eval(), [[[2], [1], [1], [1]],
                                              [[1], [3], [1], [1]],
